2d9ee31f13b7b7ca8742a2bccb951634103bc038
Duygu Baran <83429611+dduygubaran@users.noreply.github.com>
2024-02-23 01:54:17 +0300

    Arrays Solutions

--- day5-Arrays.js
+++ day5-Arrays.js
@@ -192,7 +192,6 @@ if(isHas1 === -1 ){
 // 6-) Concatenate the following two variables and store it in a fullStack variable.
 const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
 const backEnd = ['Node','Express', 'MongoDB']
-
 let fullStack = frontEnd.concat(backEnd)
 console.log(fullStack)
 
